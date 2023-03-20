52075c42f70923b88c1e19f9eabd457ce81ce0ff
Zamfirov <partner.Filip.Zamfirov@philips.com>
2023-03-20 11:56:12 +0100

    Extension container

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,121 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tic-tac-toe" is now active!');
+	context.subscriptions.push(TicTacToeEditorProvider.register(context));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tic-tac-toe.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tic-tac-toe!');
-	});
 
-	context.subscriptions.push(disposable);
+
+	//context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+
+class TicTacToeEditorProvider implements vscode.CustomTextEditorProvider {
+	
+	public static register(context: vscode.ExtensionContext): vscode.Disposable {
+		const provider = new TicTacToeEditorProvider(context);
+		const providerRegistration = vscode.window.registerCustomEditorProvider(TicTacToeEditorProvider.viewType, provider);
+		return providerRegistration;
+	}
+
+	private static readonly viewType = 'tic-tac-toe.Tic-Tac-Toe';
+
+	constructor(
+		private readonly context: vscode.ExtensionContext
+	){ }
+
+	resolveCustomTextEditor(
+		document: vscode.TextDocument, 
+		webviewPanel: vscode.WebviewPanel, 
+		token: vscode.CancellationToken): void | Thenable<void> 
+	{
+		// Setup initial content for the webview
+		webviewPanel.webview.options = {
+			enableScripts: true,
+		};
+		webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
+
+		function updateWebview() {
+			webviewPanel.webview.postMessage({
+				type: 'update',
+				text: document.getText(),
+			});
+		}
+
+		// Hook up event handlers so that we can synchronize the webview with the text document.
+		//
+		// The text document acts as our model, so we have to sync change in the document to our
+		// editor and sync changes in the editor back to the document.
+		const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
+			if (e.document.uri.toString() === document.uri.toString()) {
+				updateWebview();
+			}
+		});
+
+		// Make sure we get rid of the listener when our editor is closed.
+		webviewPanel.onDidDispose(() => {
+			changeDocumentSubscription.dispose();
+		});
+
+		updateWebview();
+	}
+
+	/**
+	 * Get the static html used for the editor webviews.
+	 */
+	private getHtmlForWebview(webview: vscode.Webview): string {
+		// // Local path to script and css for the webview
+		// const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'catScratch.js'));
+
+		// const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'reset.css'));
+
+		// const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'vscode.css'));
+
+		// const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		// 	this.context.extensionUri, 'media', 'catScratch.css'));
+
+		// Use a nonce to whitelist which scripts can be run
+		const nonce = this.getNonce();
+
+		return /* html */`
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+				Use a content security policy to only allow loading images from https or from our extension directory,
+				and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Tic Tac Toe</title>
+			</head>
+			<body>
+				<div class>
+				<iframe src='https://gfycat.com/ifr/BestMeagerHoki' frameborder='0' scrolling='no' allowfullscreen width='640' height='335'></iframe>
+				<p> <a href="https://gfycat.com/bestmeagerhoki">via Gfycat</a></p>
+				</div>
+				
+			</body>
+			</html>`;
+	}
+
+	getNonce() {
+		let text = '';
+		const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		for (let i = 0; i < 32; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
+
+}
