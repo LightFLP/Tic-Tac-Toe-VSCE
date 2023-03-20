c36f94b8f6ff8ba981e5a69a446b3469650cde00
Zamfirov <partner.Filip.Zamfirov@philips.com>
2023-03-20 16:25:10 +0100

    Move extension functionality to a command

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,123 +1,82 @@
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "tic-tac-toe" is now active!');
-	context.subscriptions.push(TicTacToeEditorProvider.register(context));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('tic-tac-toe.Start', () => {
+		// Create and show a new webview
+		const panel = vscode.window.createWebviewPanel(
+		'tic-tac-toe.Tic-Tac-Toe', // Identifies the type of the webview. Used internally
+		'Tic Tac Toe', // Title of the panel displayed to the user
+		vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		{ } 
+		);
 
+		// Setup initial content for the webview
+		panel.webview.options = {
+			enableScripts: true,
+		};
 
+		panel.webview.html = getHtmlForWebview(panel.webview, context);
 
-	//context.subscriptions.push(disposable);
+    })
+  );
+
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-
-class TicTacToeEditorProvider implements vscode.CustomTextEditorProvider {
-	
-	public static register(context: vscode.ExtensionContext): vscode.Disposable {
-		const provider = new TicTacToeEditorProvider(context);
-		const providerRegistration = vscode.window.registerCustomEditorProvider(TicTacToeEditorProvider.viewType, provider);
-		return providerRegistration;
-	}
-
-	private static readonly viewType = 'tic-tac-toe.Tic-Tac-Toe';
-
-	constructor(
-		private readonly context: vscode.ExtensionContext
-	){ }
-
-	resolveCustomTextEditor(
-		document: vscode.TextDocument, 
-		webviewPanel: vscode.WebviewPanel, 
-		token: vscode.CancellationToken): void | Thenable<void> 
-	{
-		// Setup initial content for the webview
-		webviewPanel.webview.options = {
-			enableScripts: true,
-		};
-		webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
-
-		function updateWebview() {
-			webviewPanel.webview.postMessage({
-				type: 'update',
-				text: document.getText(),
-			});
-		}
-
-		// Hook up event handlers so that we can synchronize the webview with the text document.
-		//
-		// The text document acts as our model, so we have to sync change in the document to our
-		// editor and sync changes in the editor back to the document.
-		const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
-			if (e.document.uri.toString() === document.uri.toString()) {
-				updateWebview();
-			}
-		});
-
-		// Make sure we get rid of the listener when our editor is closed.
-		webviewPanel.onDidDispose(() => {
-			changeDocumentSubscription.dispose();
-		});
-
-		updateWebview();
-	}
-
-	/**
+/**
 	 * Get the static html used for the editor webviews.
 	 */
-	private getHtmlForWebview(webview: vscode.Webview): string {
-		// // Local path to script and css for the webview
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'out', 'viewer', 'viewer.js'));
+export function getHtmlForWebview(webview: vscode.Webview, context: vscode.ExtensionContext): string {
+	// // Local path to script and css for the webview
+	const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		context.extensionUri, 'out', 'viewer', 'viewer.js'));
 
-		// const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
-		// 	this.context.extensionUri, 'media', 'reset.css'));
+	// const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
+	// 	this.context.extensionUri, 'media', 'reset.css'));
 
-		// const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
-		// 	this.context.extensionUri, 'media', 'vscode.css'));
+	// const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
+	// 	this.context.extensionUri, 'media', 'vscode.css'));
 
-		const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(
-			this.context.extensionUri, 'media', 'style.css'));
+	const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(
+		context.extensionUri, 'media', 'style.css'));
 
-		// Use a nonce to whitelist which scripts can be run
-		const nonce = this.getNonce();
+	// Use a nonce to whitelist which scripts can be run
+	const nonce = getNonce();
 
-		return /* html */`
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
+	return /* html */`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
 
-			<!--
-			Use a content security policy to only allow loading images from https or from our extension directory,
-			and only allow scripts that have a specific nonce.
-			-->
-			<!--<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-			-->
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--
+		Use a content security policy to only allow loading images from https or from our extension directory,
+		and only allow scripts that have a specific nonce.
+		-->
+		<!--<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+		-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-			<link href="${styleUri}" rel="stylesheet" />
+		<link href="${styleUri}" rel="stylesheet" />
 
-			<title>TicTacToe</title>
-		</head>
-		<body>
-			<div id='root'/>
-			<script nonce="${nonce}" src="${scriptUri}"></script>
-		</body>
-		</html>`;
+		<title>TicTacToe</title>
+	</head>
+	<body>
+		<div id='root'/>
+		<script nonce="${nonce}" src="${scriptUri}"></script>
+	</body>
+	</html>`;
+}
+
+export function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	}
-
-	getNonce() {
-		let text = '';
-		const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		for (let i = 0; i < 32; i++) {
-			text += possible.charAt(Math.floor(Math.random() * possible.length));
-		}
-		return text;
-	}
-
+	return text;
 }
